5b9a901347a4235ee834b2738a50a312bee4308c
Girihub <giridhardandikwar@gmail.com>
2019-11-05 17:18:12 +0530

    First Commit

---
+++ DesignPatterns/CreationalDesignPattern/SingletonPattern/EagerSingleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPattern.SingletonPattern
+{
+    public class EagerSingleton
+    {
+        /// <summary>
+        /// instance initialized when loading the class
+        /// </summary>
+        private static EagerSingleton instance = new EagerSingleton();
+
+        /// <summary>
+        /// Private Constructor
+        /// </summary>
+        private EagerSingleton()
+        {
+            Console.WriteLine("This is EagerSingleton Constructor");
+        }
+
+        /// <summary>
+        /// Methode to return instance
+        /// </summary>
+        /// <returns>returns instance</returns>
+        public static EagerSingleton getInstance()
+        {
+            return instance;
+        }
+    }
+}
---
+++ DesignPatterns/CreationalDesignPattern/SingletonPattern/LazySingleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPattern.SingletonPattern
+{
+    public class LazySingleton
+    {
+        /// <summary>
+        /// private instance set to null
+        /// </summary>
+        private static LazySingleton instance = null;
+
+        /// <summary>
+        /// Private constructor
+        /// </summary>
+        private LazySingleton()
+        {
+            Console.WriteLine("This is LazySingleton Constructor");
+        }
+
+        /// <summary>
+        /// Method to return instance
+        /// </summary>
+        /// <returns>returns instance</returns>
+        public static LazySingleton getInstance()
+        {
+            ////if instance is null, initialize
+            if (instance == null)
+            {
+                instance = new LazySingleton();
+            }
+            return instance;
+        }
+    }
+}
